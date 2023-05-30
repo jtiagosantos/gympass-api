058dc46688ef994924d6bd3df039db96a9e8b847
jtiagosantos <tiagosan040@gmail.com>
2023-05-29 23:13:31 -0300

    feat(auth): register jwt in app, add new variable into env

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify';
+import fastifyJwt from '@fastify/jwt';
 
 import { logs } from './logs';
 import { errorHandler } from './global/error-handler';
+import { env } from './env';
 
 import { appRoutes } from './http/routes';
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
 
 app.addHook('preHandler', logs);
 
--- src/env/index.ts
+++ src/env/index.ts
@@ -4,9 +4,14 @@ import { z } from 'zod';
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string({
-    required_error: 'DATABASE_URL is a required environment variable',
-  }),
+  DATABASE_URL: z
+    .string({
+      required_error: 'DATABASE_URL is a required environment variable',
+    })
+    .nonempty({ message: 'DATABASE_URL is a required environment variable' }),
+  JWT_SECRET: z
+    .string({ required_error: 'DATABASE_URL is a required environment variable' })
+    .nonempty({ message: 'DATABASE_URL is a required environment variable' }),
 });
 
 const _env = envSchema.safeParse(process.env);
